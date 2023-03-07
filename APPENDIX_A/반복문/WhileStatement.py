6cc2bb2af26196d6771140bbd49a8cd8cf821697
Kyungmin MIN <kyungminmin@Kyungminui-MacBookPro.local>
2023-03-07 19:53:32 +0900

    반복문 추가

---
+++ APPENDIX_A/반복문/WhileStatement.py
@@ -0,0 +1,21 @@
+i = 1
+result = 0
+
+# i 가 9보다 작거나 같을 때 아래 코드를 반복적으로 실행
+while i <= 9:
+    result += i
+    i += 1
+
+print(result)
+
+# 1부터 9까지의 수 중에서 홀수만 더하고자 할 때
+i = 1
+result = 0
+
+# i가 9보다 작거나 같을 때 아래 코드를 반복적으로 실행
+while i <= 9:
+    if i % 2 == 1:
+        result += i
+    i += 1
+
+print(result)
